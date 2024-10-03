bab6dd4f8039547701c89711fcaaac02bf2f3f89
axelinstrumental <aobscura@instrumental.net>
2024-10-03 08:11:06 -0600

    qs

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -5,8 +5,6 @@ import Image from "next/image";
 const TopBar = () => {
   return (
     <>
-
-
       <nav className="bg-white border-gray-200 dark:bg-black">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl pt-2 pb-2 pl-4 pr-4">
               <div className="flex items-center space-x-6 rtl:space-x-reverse">
@@ -30,10 +28,21 @@ const TopBar = () => {
                   </a>
               </div>
               <div className="flex items-center space-x-6 rtl:space-x-reverse">
+                  <a href="https://www.facebook.com/imcycoficial/" target="_blank" className="text-sm  text-gray-500 dark:text-gray-400 hover:underline flex flex-row">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                      <path d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" />
+                    </svg>
+                  </a>
+                  <a href="https://www.facebook.com/imcycoficial/" target="_blank" className="text-sm  text-gray-500 dark:text-gray-400 hover:underline flex flex-row mt-2">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path
+                        d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                      />
+                    </svg>
+                  </a>
               </div>
           </div>
       </nav>
-
       <nav className="bg-white border-gray-200 dark:bg-gray-900 z-50">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
