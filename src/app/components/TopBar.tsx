6c55f6a5aef3f2b44f8ea30b7f20f01c93b9a934
axelinstrumental <aobscura@instrumental.net>
2024-10-04 09:06:01 -0600

    footer

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -164,6 +164,7 @@ const TopBar = () => {
                 <button
                   id="mega-menu-full-dropdown-button"
                   data-collapse-toggle="mega-menu-full-dropdown"
+                  data-dropdown-toggle="mega-menu-full-dropdown"
                   className="font-montserrat flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700 text-sm"
                 >
                   GERENCIA TÉCNICA{" "}
