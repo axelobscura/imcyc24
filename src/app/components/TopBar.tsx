68f35258636a7259b34d1ee72fe0c5026c3598e3
axelinstrumental <aobscura@instrumental.net>
2024-10-04 19:26:48 -0600

    asociados

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -98,7 +98,7 @@ const TopBar = () => {
               <input
                 type="text"
                 id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-900 bg-gray-90 focus:ring-gray-900 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-900 dark:placeholder-gray-900 dark:text-white dark:focus:ring-gray-900 dark:focus:border-gray-900"
                 placeholder="Buscar..."
               />
             </div>
