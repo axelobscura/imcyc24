c0a76a4efa8c71b536f4bed9849fc4370f929611
axelinstrumental <aobscura@instrumental.net>
2024-10-09 17:57:35 -0600

    test

--- src/app/laboratorio-de-ensayos/page.tsx
+++ src/app/laboratorio-de-ensayos/page.tsx
@@ -31,18 +31,18 @@ export default function LaboratorioDeEnsayos() {
   ];
   return (
     <>
-      <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 bg-opacity-80 bg-[url('/gerencia-tecnica.jpg')] bg-gray-900 bg-blend-multiply">
+      <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 bg-opacity-80 bg-[url('/gerencia-tecnica.jpg')] bg-cover bg-gray-900 bg-blend-multiply">
         <div className="mx-auto w-full max-w-screen-xl p-4 pt-20 sm:pt-80 pb-0 lg:py-20">
           <ul className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr_1fr_1fr] gap-8 pt-0 sm:pt-20">
             {servicios.map((servicio) => (
               <li>
-                <div className="max-w-sm bg-gray-900 border border-gray-800 rounded-lg shadow hover:opacity-70">
+                <div className="max-w-sm bg-gray-900 border border-gray-700 hover:border-gray-500 rounded-sm shadow hover:opacity-70">
                     <a href="#">
-                        <img className="rounded-t-lg" src={servicio.imagen} alt={servicio.nombre} />
+                        <img className="rounded-t-sm" src={servicio.imagen} alt={servicio.nombre} />
                     </a>
                     <div className="p-5 text-center">
                         <a href="#">
-                            <h5 className="font-montserrat mb-2 text-2xl font-bold tracking-tight text-white uppercase font-light">{servicio.nombre}</h5>
+                            <h5 className="font-montserrat mb-2 text-2xl tracking-tight text-white uppercase font-light">{servicio.nombre}</h5>
                         </a>
                     </div>
                 </div>
