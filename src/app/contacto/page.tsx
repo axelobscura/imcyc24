2e37cad968990924c15f8d837c13fc8960954bc6
axelinstrumental <aobscura@instrumental.net>
2024-11-04 16:35:32 -0600

    labos

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -6,7 +6,7 @@ export default function Contacto() {
     <div>
       <div className="flex items-center justify-items-center min-h-screen p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)]  bg-gradient-to-t from-gray-900 to-gray-800 z-10 bg-cover bg-center bg-no-repeat">
         <div className="mx-auto w-full max-w-screen-xl p-4 pt-20 sm:pt-80 pb-0 lg:py-20">
-          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 w-full">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 w-full text-gray-200">
             <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-5 items-start justify-items-stretch pt-20 w-full">
               <div className="bg-gray-800 bg-opacity-50 bg-blend-multiply w-full p-5 sm:pl-5 md:pl-5 lg:pl-8 xl:pl-50 sm:mb-5 border border-gray-700">
                 <h1 className="flex items-center font-montserrat text-2xl text-white pt-0 sm:pl-50 uppercase font-bold pb-3">
