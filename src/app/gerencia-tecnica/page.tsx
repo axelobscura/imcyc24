faefd5d0f6d96de053e49ac275eaa854ab32aab3
axelinstrumental <aobscura@instrumental.net>
2024-11-18 19:29:17 -0600

    historia

--- src/app/gerencia-tecnica/page.tsx
+++ src/app/gerencia-tecnica/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function GerenciaTecnica() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/qs2.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/cert.jpeg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-fixed bg-no-repeat">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0">
-        <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-0 items-center justify-items-center">
+        <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-0 items-start justify-items-center">
           <div className="bg-gray-900 bg-opacity-50 bg-blend-multiply w-full p-10 sm:pl-26 md:pl-26 lg:pl-40">
             <h1 className="text-4xl font-bold text-white pt-10 sm:pl-190">
               ¿Qué es la GERENCIA TÉCNICA?
@@ -13,27 +13,27 @@ export default function GerenciaTecnica() {
             <hr />
             <br />
             <p className="text-justify font-montserrat text-sm text-white">
-              La Gerencia Técnica del Instituto tiene como objetivo representar
+              <strong>La Gerencia Técnica del Instituto tiene como objetivo representar
               una autoridad técnica reconocida en materia del cemento y del
-              concreto. Esta Gerencia se encuentra integrada por seis áreas, que
-              ofrecen asesorías técnicas y diversos servicios de Evaluación de
+              concreto</strong>. Esta Gerencia se encuentra integrada por seis áreas, que
+              ofrecen asesorías técnicas y <strong>diversos servicios de Evaluación de
               la Conformidad, que son Laboratorio de Ensayos, Laboratorio de
               Calibración, Proveedor de Ensayos de Aptitud y Organismo de
-              Certificación de Producto.
+              Certificación de Producto</strong>.
               <br />
               <br />
               El laboratorio de ensayos se encuentra conformado por el
-              laboratorio de concreto y el laboratorio de cemento, ambos
+              laboratorio de concreto y el laboratorio de cemento, <strong>ambos
               laboratorios cuentan con un sistema de gestión acreditado ante la
               entidad mexicana de acreditación (ema) en las ramas de
-              construcción y metalmecánica.
+              construcción y metalmecánica</strong>.
               <br />
               <br />
-              En la rama de construcción, el laboratorio cuenta con 18 métodos
+              En la rama de construcción, <strong>el laboratorio cuenta con 18 métodos
               acreditados en la subrama de cemento; 15 métodos acreditados para
               la subrama de concreto; 9 métodos de agregados y 6 métodos
               acreditados para prefabricados; en muchos de estos métodos, somos
-              el único laboratorio acreditado a nivel nacional, como es el caso
+              el único laboratorio acreditado a nivel nacional</strong>, como es el caso
               de las pruebas para concreto reforzado con fibras, ensayos de
               paneles, sistemas de losas prefabricadas, pruebas físicas de
               adhesivos, asesorías en Estructuras, OCPER, entre otros.
@@ -79,6 +79,7 @@ export default function GerenciaTecnica() {
               anual de los programas se publica en diciembre de cada año en la
               página del Instituto.
               <br />
+              <br />
               El Organismo de Certificación de Producto es el Organismo de
               Evaluación de la Conformidad más reciente del Instituto, cuenta
               con acreditación ante la entidad mexicana de acreditación para
@@ -100,7 +101,7 @@ export default function GerenciaTecnica() {
               actividades clave en la industria.
             </p>
           </div>
-          <div className="w-full flex justify-center">
+          <div className="w-full flex justify-center pt-20">
             <Image
               src="/gerencia-tecnica.jpg"
               alt="IMCYC"
