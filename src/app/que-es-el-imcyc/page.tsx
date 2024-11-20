440a2a3b82dfc3ea57eaba177f6638bf3f0c802e
axelinstrumental <aobscura@instrumental.net>
2024-11-20 17:25:16 -0600

    slider

--- src/app/que-es-el-imcyc/page.tsx
+++ src/app/que-es-el-imcyc/page.tsx
@@ -5,11 +5,12 @@ import Image from "next/image";
 export default function QueEsElImcyc() {
   return (
     <div>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/qs2.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0">
-          <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-0 items-center justify-items-center">
+      <div className="font-[family-name:var(--font-geist-sans)] bg-[url('/qs2.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+      <div className="mx-auto w-full max-w-screen-xl p-4 pt-90 sm:pt-150 pb-0 lg:py-20">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 pt-160">
+          <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-0 items-center justify-items-center mt-28">
             <div className="bg-gray-900 bg-opacity-50 bg-blend-multiply w-full p-10 sm:pl-5 md:pl-5 lg:pl-8 xl:pl-50">
-              <h1 className="text-4xl font-bold text-white pt-10 sm:pl-50">
+              <h1 className="text-4xl font-bold text-white pt-0 sm:pl-50">
                 ¿Qué es el IMCYC?
               </h1>
               <br />
@@ -76,6 +77,7 @@ export default function QueEsElImcyc() {
             </div>
           </div>
         </main>
+        </div>
       </div>
     </div>
   );
