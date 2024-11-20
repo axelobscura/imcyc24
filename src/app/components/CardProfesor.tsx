3081e2031ba01f7890779c5add5ffbcf08297c1a
axelinstrumental <aobscura@instrumental.net>
2024-11-20 13:56:14 -0600

    profesores

--- src/app/components/CardProfesor.tsx
+++ src/app/components/CardProfesor.tsx
@@ -1,6 +1,6 @@
 
 "use client";
-import { Card, Dropdown } from "flowbite-react";
+import { Card } from "flowbite-react";
 import Image from "next/image";
 
 export function CardProfesor({nombre, imagen, certificado, descripcion} : {
