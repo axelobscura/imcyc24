a36778f517d04188558d1ac3477e03b17a61a374
axelinstrumental <aobscura@instrumental.net>
2024-11-20 19:36:01 -0600

    slider

--- src/app/sellos-de-calidad/page.tsx
+++ src/app/sellos-de-calidad/page.tsx
@@ -8,8 +8,8 @@ export default function SellosDeCalidad() {
   const [sellos, setSellos] = useState("ollas");
   return (
     <div>
-      <div className="flex items-center justify-items-center min-h-screen p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/sellos/ollas.jpg')] bg-fixed bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
-        <div className="mx-auto w-full max-w-screen-xl p-4 pt-20 sm:pt-80 pb-0">
+      <div className="flex p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/sellos/ollas.jpg')] bg-fixed bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+        <div className="mx-auto w-full max-w-screen-xl p-4 pt-20 sm:pt-30 pb-0">
           <main className="flex flex-col gap-8 row-start-2 items-center mx-auto max-w-screen-xl sm:items-start z-0 w-full text-white">
             <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr] gap-0 items-start justify-items-stretch pt-20 w-full">
               <div className="bg-gray-900 bg-opacity-50 bg-blend-multiply w-full p-5 sm:pl-5 md:pl-5 lg:pl-8 xl:pl-50 sm:mb-5">
@@ -48,7 +48,7 @@ export default function SellosDeCalidad() {
                   <div className="pb-5">
                     <h3 className="font-montserrat text-2xl font-100 p-2 flex items-center pt-5 font-bold"><BiCross className="mr-3" /> OLLAS REVOLVEDORAS DE CONCRETO</h3>
                     <h4 className="font-montserrat ml-5 font-bold">SELLO DE CALIDAD IMCYC</h4>
-                    <p className="font-montserrat text-justify p-5">Trabajar para la profesionalización de la industria es un interés común de todo aquel que participa en las áreas relativas al cemento y el concreto. Por ello El IMCYC ha buscado la manera de reconocer los esfuerzos de aquellas compañías que tienen ese objetivo como parte de su esencia. Con esa visión el instituto ha creado un nuevo producto que se identifica con el nombre de “Sello de Garantía IMCYC”. Es una estrategia que busca dar certidumbre sobre la calidad de un producto, proceso y/o servicio y que tiene un carácter no obligatorio para su aplicación y cumplimiento, en opinión de esta institución este carácter le da un mayor valor para quién lo recibe, ya que deja claro su compromiso con la calidad de sus productos.</p>
+                    <p className="font-montserrat text-justify p-5">Trabajar para la profesionalización de la industria es un interés común de todo aquel que participa en las áreas relativas al cemento y el concreto.<br/><br/>Por ello El IMCYC ha buscado la manera de reconocer los esfuerzos de aquellas compañías que tienen ese objetivo como parte de su esencia. Con esa visión el instituto ha creado un nuevo producto que se identifica con el nombre de “Sello de Garantía IMCYC”.<br/><br/>Es una estrategia que busca dar certidumbre sobre la calidad de un producto, proceso y/o servicio y que tiene un carácter no obligatorio para su aplicación y cumplimiento, en opinión de esta institución este carácter le da un mayor valor para quién lo recibe, ya que deja claro su compromiso con la calidad de sus productos.</p>
                     <h4 className="font-montserrat ml-5 font-bold">LAS CARACTERISTICAS PRICIPALES DE ESTE ESFUERZO Y QUE SON IMPORTANTES DE DISTINGUIR SON:</h4>
                     <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr_1fr_1fr_1fr] items-start pt-5 ml-5 mr-5 mb-5">
                       <div>
