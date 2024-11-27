125e311e6f3f386196ac6d8c7210cf90ab63277b
axelinstrumental <aobscura@instrumental.net>
2024-11-26 19:41:53 -0600

    linkedin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
       >
         <TopBar/>
         {children}
-        {/* <Membresias /> */}
+        <Membresias />
         <Footer/>
       </body>
     </html>
