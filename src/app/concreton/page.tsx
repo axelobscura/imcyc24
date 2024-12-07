cba05a760ea090507822faa474cb17cee633cf5c
axelinstrumental <aobscura@instrumental.net>
2024-12-07 12:07:24 -0600

    concreton

--- src/app/concreton/page.tsx
+++ src/app/concreton/page.tsx
@@ -19,7 +19,7 @@ export default function Concreton() {
                   className="mb-5"
                 />
                 <h1 className="font-montserrat text-4xl text-white pt-0 sm:pl-50 uppercase font-bold text-center">
-                  Asistente Concreton IMCYC
+                  Asistente Concretón IMCYC
                 </h1>
                 <p className="font-montserrat text-center">Asistente en tecnología del Concreto y del Cemento</p>
                 <p className="font-montserrat text-tiny text-center">Instituto Mexicano del Cemento y del Concreto A.C.</p>
@@ -33,8 +33,8 @@ export default function Concreton() {
                         </div>
                         <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-5.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-4" placeholder="Ingrese su consulta..." required />
                     </div>
-                    <button type="submit" className="p-5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <button type="submit" className="p-5 ms-2 text-sm font-medium text-white bg-slate-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                         <span className="sr-only">Search</span>
