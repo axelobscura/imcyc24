58f9ae5e1b482978437ea420bff679bb453f441a
axelinstrumental <aobscura@instrumental.net>
2024-12-07 12:09:15 -0600

    concreton

--- src/app/concreton/page.tsx
+++ src/app/concreton/page.tsx
@@ -31,7 +31,7 @@ export default function Concreton() {
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                           <RiRobot2Line size={25} className="text-slate-900" />
                         </div>
-                        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-5.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-4" placeholder="Ingrese su consulta..." required />
+                        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-12 p-5.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 p-4" placeholder="Ingrese su consulta..." required />
                     </div>
                     <button type="submit" className="p-5 ms-2 text-sm font-medium text-white bg-slate-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
