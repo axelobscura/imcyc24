a3cc5fc13fbdf1d5d6346ceb6b9e3cd7330a77ca
axelinstrumental <aobscura@instrumental.net>
2024-12-10 11:29:46 -0600

    text-white

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -13,7 +13,7 @@ import { AiOutlinePlusCircle } from "react-icons/ai";
 function Menu() {
   return (
     <MegaMenu className="bg-gray-900 bg-opacity-50 dark:bg-gray-900 z-50 shadow-lg dark:bg-opacity-50 border-b border-slate-700">
-      <div className="w-full flex flex-wrap justify-between items-center mx-auto max-w-screen-xl pt-2 pb-2 pl-4 pr-4 text-white">
+      <div className="w-full flex flex-wrap justify-between items-center mx-auto max-w-screen-xl pt-2 pb-2 pl-4 pr-4 text-white menu-responsivo">
         <NavbarBrand href="/">
           <Image
             src="/logo-imcyc.svg"
@@ -33,7 +33,7 @@ function Menu() {
           <MegaMenuDropdown
             className="bg-gray-900 bg-opacity-95 border-none m-0 ml-0 text-white"
             toggle={
-              <div className="font-montserrat whitespace-nowrap text-white bg-blue-700 md:bg-transparent md:text-gray-100 md:p-0 md:dark:text-gray-100 text-sm uppercase flex items-center">
+              <div className="font-montserrat whitespace-nowrap py-2 text-white lg:bg-blue-700 rounded md:bg-transparent md:text-gray-100 md:p-0 md:dark:text-gray-100 text-sm uppercase flex items-center mr-0 pr-0">
                 Gerencia Técnica
               </div>
             }
@@ -127,7 +127,7 @@ function Menu() {
             className="p-0 m-0 top-96 bg-gray-900 bg-opacity-90 border-none ml-0 mr-0 pl-0 pr-0 text-white"
             style={{ top: "196px" }}
             toggle={
-              <div className="font-montserrat whitespace-nowrap py-2 px-2 text-white bg-blue-700 rounded md:bg-transparent md:text-gray-100 md:p-0 md:dark:text-gray-100 text-sm uppercase flex items-center mr-0 pr-0">
+              <div className="font-montserrat whitespace-nowrap py-2 text-white lg:bg-blue-700 rounded md:bg-transparent md:text-gray-100 md:p-0 md:dark:text-gray-100 text-sm uppercase flex items-center mr-0 pr-0">
                 Enseñanza
               </div>
             }
