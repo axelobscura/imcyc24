73982ce38f875eab28e15ff0fe03cbb78e10da07
axelinstrumental <aobscura@instrumental.net>
2024-12-09 20:59:54 -0600

    loader

--- src/app/mision/page.tsx
+++ src/app/mision/page.tsx
@@ -1,11 +1,13 @@
 export default function Mision() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] sm:grid-rows-[1fr_1fr_1fr] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('https://www.webinarsenconcreto.com/images/2022/fondo1.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
-      <main className="">
-        <h2 className="text-4xl text-white font-montserrat block font-bold">Misión</h2>
-        <p className="text-white font-montserrat text-900 text-2xl">Crear una cultura tecnológica del cemento y del concreto con el fin de impulsar su mayor uso en forma óptima, mediante la promoción y capacitación sobre las mejores prácticas a nivel mundial, para generar beneficios a la Industria y a la Sociedad.</p>
-        <h2 className="text-4xl text-white font-montserrat block font-bold">Visión</h2>
-        <p className="text-white font-montserrat text-900 text-2xl">Ser la Institución lider en Latinoamérica en la diculgación científica y tecnológica, en la enseñanza, consultoría y la autoridad técnica reconocida en materia de Cemento y Concreto.</p>
+    <div className="grid grid-rows-[20px_1fr_20px]items-center justify-items-center p-8 font-[family-name:var(--font-geist-sans)] bg-[url('https://www.webinarsenconcreto.com/images/2022/fondo1.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+      <main className="mx-auto max-w-screen-xl gap-8 row-start-2 items-center sm:items-start z-0 px-5 py-60">
+        <h2 className="text-4xl text-white font-montserrat block font-bold pb-4">Misión</h2>
+        <p className="text-white font-montserrat text-900 text-2xl pb-5">Crear una cultura tecnológica del cemento y del concreto con el fin de impulsar su mayor uso en forma óptima, mediante la promoción y capacitación sobre las mejores prácticas a nivel mundial, para generar beneficios a la Industria y a la Sociedad.</p>
+        <h2 className="text-4xl text-white font-montserrat block font-bold pb-4">Visión</h2>
+        <p className="text-white font-montserrat text-900 text-2xl pb-5">Ser la Institución lider en Latinoamérica en la diculgación científica y tecnológica, en la enseñanza, consultoría y la autoridad técnica reconocida en materia de Cemento y Concreto.</p>
+        <h2 className="text-4xl text-white font-montserrat block font-bold pb-4">Política de calidad</h2>
+        <p className="text-white font-montserrat text-900 text-2xl pb-5">En el IMCYC estamos comprometidos a proveer servicios que satisfagan las expectativas de nuestros clientes, con base en la mejora continua de nuestro Sistema de Gestión de Calidad, manteniendo la imparcialidad en la prestación de nuestros servicios.</p>
       </main>
     </div>
   );
