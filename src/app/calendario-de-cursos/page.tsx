971c6a142142426deba42639a2be7a1cd7d99e5b
axelinstrumental <aobscura@instrumental.net>
2025-01-13 10:35:33 -0600

    calendario

--- src/app/calendario-de-cursos/page.tsx
+++ src/app/calendario-de-cursos/page.tsx
@@ -17,7 +17,7 @@ export default function CalendarioDeCursos() {
 
   return (
     <div>
-      <div className="flex items-center justify-items-center p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/revista.jpeg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+      <div className="flex items-center justify-items-center p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/revista.jpeg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat bg-fixed">
         <div className="mx-auto w-full max-w-screen-xl p-4 pt-20 sm:pt-80 pb-0 lg:py-40">
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 w-full">
             <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr] gap-0 items-center justify-items-stretch pt-20 w-full">
@@ -31,14 +31,14 @@ export default function CalendarioDeCursos() {
                 <div>
                   {posts.map((post, index) => (
                     <div key={index} className="font-montserrat grid grid-cols-[1fr_1fr_5fr_1fr] gap-4 bg-gray-950 bg-opacity-50 bg-blend-multiply p-2 my-1 items-center">
-                      <p className='flex items-center justify-center text-2xl font-bold text-white'>{post[0]}</p>
+                      <p className='flex items-center justify-left text-2xl font-bold text-white'>{post[0]}</p>
                       <p className='flex items-center justify-center text-2xl font-bold text-white'>{post[1]}</p>
                       <div className='bg-gray-800 bg-opacity-50 p-3'>
                         <p className='text-justify text-medium mt-2 text-white'>{post[5]}</p>
                         <p className="text-2xl font-bold text-white">{post[2]}</p>
                         <p className='text-justify text-medium mt-2 text-white'>{post[3]}</p>
                       </div>
-                      <p className='flex items-center justify-center text-2xl font-bold text-white'>${new Intl.NumberFormat('en-US').format(post[4])}.00</p>
+                      <p className='flex items-center justify-left text-2xl font-bold text-white'>${new Intl.NumberFormat('en-US').format(post[4])}.00</p>
                     </div>
                   ))}
                 </div>
