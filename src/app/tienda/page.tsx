9fda37d0d5f066b9e27cb0ab781a70f5fce75ae2
axelinstrumental <aobscura@instrumental.net>
2025-04-10 20:47:22 -0600

    responsive

--- src/app/tienda/page.tsx
+++ src/app/tienda/page.tsx
@@ -351,7 +351,7 @@ export default function Tienda() {
   return (
     <div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('https://www.webinarsenconcreto.com/images/2022/fondo1.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
-        <main className="grid grid-cols-5 gap-8 row-start-2 items-center sm:items-start z-0" style={{
+        <main className="grid grid-cols-1 sm:grid-cols-5 gap-8 row-start-2 items-center sm:items-start z-0" style={{
           marginTop: "100px",
           marginBottom: "100px",
           padding: "20px",
