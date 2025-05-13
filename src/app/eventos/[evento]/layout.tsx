cb526b77fdf0b10443f8c56fc9b0290e1a05cea2
axelinstrumental <aobscura@instrumental.net>
2025-05-13 11:08:22 -0600

    metas

---
+++ src/app/eventos/[evento]/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { GoogleTagManager } from '@next/third-parties/google';
+import localFont from "next/font/local";
+import TopBar from "../../components/TopBar";
+import Membresias from "../../components/Membresias";
+import Footer from "../../components/Footer";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "../../globals.css";
+
+const geistSans = localFont({
+  src: "../../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Seminario pisos industriales de concreto | Instituto Mexicano del Cemento y del Concreto A.C.",
+  description: "El Instituto Mexicano del Cemento y del Concreto A.C. es una organización sin fines de lucro dedicada a promover el uso óptimo del cemento y del concreto en la construcción, contribuyendo a la mejora del desempeño profesional, el desarrollo industrial y los beneficios económicos para la sociedad.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      <TopBar/>
+      {children}
+      <Membresias />
+      <Footer/>
+    </body>
+  );
+}
