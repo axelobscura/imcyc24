030a1be754d53138b23407e2bc8c51ab7ebad342
axelinstrumental <aobscura@instrumental.net>
2025-05-16 17:27:51 -0600

    descuento

--- src/app/eventos/[evento]/layout.tsx
+++ src/app/eventos/[evento]/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import TopBar from "../../components/TopBar";
 import Membresias from "../../components/Membresias";
 import Footer from "../../components/Footer";
 import "react-image-gallery/styles/css/image-gallery.css";
 import "../../globals.css";
-
-const geistSans = localFont({
-  src: "../../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Seminario pisos industriales de concreto | Instituto Mexicano del Cemento y del Concreto A.C.",
