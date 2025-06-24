ddcad9db71789ac86033c96134e025c822979896
Axel Laurent <axosar@gmail.com>
2025-06-24 17:59:02 -0600

    banner

--- src/app/eventos/[evento]/page.tsx
+++ src/app/eventos/[evento]/page.tsx
@@ -7,7 +7,7 @@ export default function Articulo() {
   const params = useParams<{ evento: string }>();
   return (
     <div>
-      <div className="flex items-center justify-items-center p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/seminario-pisos-industriales-concreto-monterrey-imcyc-2025.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+      <div className="flex items-center justify-items-center p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/bkgs/tecnologia.jpeg')] bg-gray-700 bg-blend-multiply z-10 bg-fixed bg-center bg-no-repeat">
         <div className="mx-auto w-full max-w-screen-xl p-4 md:pt-40 sm:pt-0 pb-0 lg:py-40">
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 w-full">
             <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr] gap-0 items-center justify-items-stretch pt-20 w-full">
