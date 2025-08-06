d01606c8ae9619b28a43de7d59561187164d5252
Axel Laurent <axosar@gmail.com>
2025-08-06 10:01:31 -0600

    test

--- lib/db.ts
+++ lib/db.ts
@@ -17,7 +17,7 @@ export async function query(
     const results = await db.query(q, values)
     await db.end()
     return results
-  } catch {
-    console.error('Database query error:', q, values)
+  } catch (e: any) {
+    throw Error(e.message)
   }
 }
