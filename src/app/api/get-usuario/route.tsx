41c413f9ab83f97aa3a82258a4756a76c7a59b15
Axel Laurent <axosar@gmail.com>
2025-08-07 11:58:17 -0600

    form-sql

--- src/app/api/get-usuario/route.tsx
+++ src/app/api/get-usuario/route.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { query } from '../../../../lib/db'
+import { query } from '../../../../lib/db';
 
 export async function GET(request: Request) {
   const params = request.url.split('?')[1];
@@ -7,9 +7,7 @@ export async function GET(request: Request) {
   const elPwd = params.split('&')[1].split('=')[1];
   console.log("Email: "+elEmail+" pw: "+elPwd);
   try {
-    const results = await query(`
-      SELECT * FROM usuarios WHERE email='${elEmail}' AND password='${elPwd}'
-    `) as { email: string; password: string; }[];
+    const results = await query(`SELECT * FROM usuarios WHERE email=${elEmail} AND password=${elPwd}`) as { email: string; password: string; }[];
     console.log("Query results:", results);
     return NextResponse.json(results);
   } catch (e: unknown) {
--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -4,7 +4,8 @@ import { Carousel } from "flowbite-react";
 import Image from "next/image";
 
 export function Slider({imagenes}: {
-  imagenes: string[]
+  imagenes: string[],
+  link?: string,
 }) {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
