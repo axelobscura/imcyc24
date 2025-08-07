ea6f9b1a66afb13a12d5ccec6ed04a3222a1cc89
Axel Laurent <axosar@gmail.com>
2025-08-07 12:26:25 -0600

    mangle

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,38 @@ const nextConfig = {
   images: {
     domains: ['imcyc.net'],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        minimizer: [
+          ...config.optimization.minimizer,
+        ],
+      }
+      
+      // Find TerserPlugin and modify options
+      const terserPlugin = config.optimization.minimizer.find(
+        plugin => plugin.constructor.name === 'TerserPlugin'
+      )
+      
+      if (terserPlugin) {
+        terserPlugin.options.terserOptions = {
+          ...terserPlugin.options.terserOptions,
+          mangle: {
+            ...terserPlugin.options.terserOptions.mangle,
+            keep_fnames: true, // Keep function names
+          },
+          compress: {
+            ...terserPlugin.options.terserOptions.compress,
+            keep_fnames: true, // Keep function names in compression
+          }
+        }
+      }
+    }
+    
+    return config
+  }
 };
 
 export default nextConfig;
