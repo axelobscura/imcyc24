ab7c99f663a255039994b281b80360100cca0566
Axel Laurent <axosar@gmail.com>
2025-08-07 12:03:00 -0600

    form-sql

--- src/app/api/get-usuario/route.tsx
+++ src/app/api/get-usuario/route.tsx
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 import { query } from '../../../../lib/db';
 
-export async function GET(request: Request) {
+export function GET(request: Request) {
   const params = request.url.split('?')[1];
   const elEmail = params.split('&')[0].split('=')[1];
   const elPwd = params.split('&')[1].split('=')[1];
   console.log("Email: "+elEmail+" pw: "+elPwd);
   try {
-    const results = await query(`SELECT * FROM usuarios WHERE email=${elEmail} AND password=${elPwd}`) as { email: string; password: string; }[];
+    const results = query(`SELECT * FROM usuarios WHERE email=${elEmail} AND password=${elPwd}`);
     console.log("Query results:", results);
     return NextResponse.json(results);
   } catch (e: unknown) {
