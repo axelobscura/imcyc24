9cbd8dded3ee745dab54b6c9827db355090a9b33
Axel Laurent <axosar@gmail.com>
2025-08-14 19:30:19 -0600

    404

---
+++ src/app/not.found.tsx
@@ -0,0 +1,15 @@
+//FLOWBITE - https://flowbite.com/docs/components/navbar/
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <div className="font-[family-name:var(--font-geist-sans)] bg-[url('/qs2.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+      <div className="mx-auto w-full max-w-screen-xl p-4 pt-90 sm:pt-150 pb-0 lg:py-40">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 pt-160">
+          <h2>EN CONSTRUCCIÓN</h2>
+        </main>
+        </div>
+      </div>
+    </div>
+  );
+}
