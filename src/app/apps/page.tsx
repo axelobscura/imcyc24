1b8e385032359357d54bb2ac4a6c0c61f149606d
Axel Laurent <axosar@gmail.com>
2025-09-09 19:05:36 -0600

    apps-layout

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -4,6 +4,7 @@
 import { useState, useEffect } from 'react'
 import Image from "next/image";
 import Loader from '../components/Loader';
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 export default function Apps() {
   const [loading, setLoading] = useState(false);
@@ -56,12 +57,16 @@ export default function Apps() {
   
   return (
     <div>
-      <div className="grid grid-rows-[20px_1fr_20px] p-8 pb-20 gap-16 sm:p-20 min-h-screen font-[family-name:var(--font-geist-sans)] bg-[url('https://www.webinarsenconcreto.com/images/2022/fondo1.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
+      <div className="p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('https://www.webinarsenconcreto.com/images/2022/fondo1.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
         <div className="mx-auto w-full max-w-screen-xl p-4 mt-10 sm:pt-120 pb-0 lg:py-90">
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 w-full">
             <div className="grid grid-cols-[1fr] sm:grid-cols-[1fr] gap-0 items-center justify-items-stretch pt-20 w-full">
-              <h1 className="font-montserrat text-4xl font-light text-white text-center uppercase">Aplicaciones para la construcción</h1>
-              <div className="grid grid-cols-[1fr] md:grid-cols-[1fr_1fr_1fr] py-10 gap-5">
+              <h1 className="font-montserrat text-4xl font-light text-white text-center uppercase pb-7">Aplicaciones para la construcción</h1>
+              <h3 className='flex items-center font-montserrat text-2xl font-light text-white text-left uppercase my-4 py-2 border-t-[1px] border-t-white border-t-solid'><IoIosArrowDroprightCircle className='mr-1' /> Plataformas educativas</h3>
+              <h3 className='flex items-center font-montserrat text-2xl font-light text-white text-left uppercase my-4 py-2 border-t-[1px] border-t-white border-t-solid'><IoIosArrowDroprightCircle className='mr-1' /> Certificaciones | Sellos de calidad</h3>
+              <h3 className='flex items-center font-montserrat text-2xl font-light text-white text-left uppercase my-4 py-2 border-t-[1px] border-t-white border-t-solid'><IoIosArrowDroprightCircle className='mr-1' /> Industria</h3>
+              <h3 className='flex items-center font-montserrat text-2xl font-light text-white text-left uppercase my-4 py-2 border-t-[1px] border-t-white border-t-solid'><IoIosArrowDroprightCircle className='mr-1' /> Personalizadas</h3>
+              <div className="grid grid-cols-[1fr] md:grid-cols-[1fr_1fr_1fr] py-1 gap-5">
                 {!loading && aplicaciones.map((app) => (
                   <a key={app.id} href={app.url} target='_blank' className="grid grid-cols-[1fr_3fr] items-center w-full p-2 bg-gray-900 border border-gray-800 rounded-lg text-gray-100 shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:text-gray-900">
                     <div className="bg-gray-100 mr-5 text-center relative rounded-full">
