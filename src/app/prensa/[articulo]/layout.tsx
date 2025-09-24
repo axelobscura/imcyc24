c7323cc0340322245fb8f43621cadec1fa40e2d8
Axel Laurent <axosar@gmail.com>
2025-09-24 13:39:46 -0600

    test

---
+++ src/app/prensa/[articulo]/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import TopBar from "../../components/TopBar";
+import Membresias from "../../components/Membresias";
+import Footer from "../../components/Footer";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "../../globals.css";
+
+export const metadata: Metadata = {
+  title: "PRENSA | Instituto Mexicano del Cemento y del Concreto A.C.",
+  description: "El Instituto Mexicano del Cemento y del Concreto A.C. es una organización sin fines de lucro dedicada a promover el uso óptimo del cemento y del concreto en la construcción, contribuyendo a la mejora del desempeño profesional, el desarrollo industrial y los beneficios económicos para la sociedad.",
+  openGraph: {
+    title: "PRENSA | Instituto Mexicano del Cemento y del Concreto A.C.",
+    description: "El Instituto Mexicano del Cemento y del Concreto A.C. es una organización sin fines de lucro dedicada a promover el uso óptimo del cemento y del concreto en la construcción, contribuyendo a la mejora del desempeño profesional, el desarrollo industrial y los beneficios económicos para la sociedad.",
+    url: "https://www.imcyc.com.mx/prensa",
+    siteName: "PRENSA | Instituto Mexicano del Cemento y del Concreto A.C.",
+    images: [
+      {
+        url: 'https://imcyc.net/images/imcyc-evento-profesionalizacion-industria.jpg', // Must be an absolute URL
+        width: 800,
+        height: 600,
+        alt: 'Instituto Mexicano del Cemento y del Concreto A.C.',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@imcyc',
+    creator: '@imcyc',
+    title: 'Instituto Mexicano del Cemento y del Concreto A.C.',
+    description: 'This is a description of my awesome site.',
+    images: 'https://imcyc.net/images/imcyc-evento-profesionalizacion-industria.jpg', // Must be an absolute URL
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <TopBar/>
+      {children}
+      <Membresias />
+      <Footer/>
+    </>
+  );
+}
