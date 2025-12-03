bd5494965b031ea1295c05de0f7bd76d07899f02
Axel Laurent <axosar@gmail.com>
2025-12-03 17:09:28 -0600

    calendario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,43 +38,6 @@ export default function Home() {
             }}
           >
             <SwiperSlide 
-              className="bg-[url('/prensa/diplomado.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat h-auto"
-              style={{ height: '100vh' }}
-            >
-              <div className="py-0 px-0 mx-auto max-w-screen-xl text-center z-10 relativepb-36 pt-36">
-                <h1 className="font-montserrat pt-40 pb-0 text-white text-2xl">INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
-                <h2 className="font-montserrat mb-2 text-3xl font-light tracking-tight leading-none text-white md:text-2xl lg:text-2xl dark:text-white">
-                CAPACÍTATE CON LOS LÍDERES DEL CONCRETO</h2>
-                <h3 className="font-montserrat pt-10 pb-2 text-white font-bold text-4xl md:text-2xl lg:text-5xl">DIPLOMADO EN TECNOLOGÍA DEL CONCRETO</h3>
-                <h4 className="font-montserrat pb-10 text-white text-2xl">INICIA EL 30 DE OCTUBRE 2025</h4>
-                <p className="font-montserrat mb-16 text-xs font-light text-white lg:text-xl sm:px-14 lg:px-24 dark:text-gray-200">Forma parte del diplomado que construye el conocimiento del mañana.<br/>10 módulos mensuales hasta octubre 2026.
-                </p>
-                <Link
-                  href="/eventos/diplomado-tecnologia-del-concreto"
-                  className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-2 text-sm text-blue-700 rounded-full bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 mr-3"
-                >
-                  <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
-                    MÁS INFORMACIÓN
-                  </span>{" "}
-                  <svg
-                    className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 9 4-4-4-4"
-                    />
-                  </svg>
-                </Link>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide 
               className="bg-[url('https://www.webinarsenconcreto.com/images/2022/fondo1.jpg')] bg-gray-500 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat h-auto"
               style={{ height: '100vh' }}
             >
@@ -133,7 +96,7 @@ export default function Home() {
                   className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-green-700 rounded-full bg-yellow-900 dark:text-blue-300 hover:bg-yellow-200 dark:hover:bg-blue-800"
                 >
                   <span className="text-xs bg-yellow-600 rounded-full text-white px-4 py-1.5 me-3">
-                    Calendario de Organismo de Certificación de Personas 2025
+                    Calendario de Organismo de Certificación de Personas 2026
                   </span>{" "}
                   <svg
                     className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
@@ -155,6 +118,44 @@ export default function Home() {
                 </p>
               </div>
             </SwiperSlide>
+            <SwiperSlide 
+              className="bg-[url('/prensa/diplomado.jpg')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat h-auto"
+              style={{ height: '100vh' }}
+            >
+              <div className="py-0 px-0 mx-auto max-w-screen-xl text-center z-10 relativepb-36 pt-36">
+                <h1 className="font-montserrat pt-40 pb-0 text-white text-2xl">INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
+                <h2 className="font-montserrat mb-2 text-3xl font-light tracking-tight leading-none text-white md:text-2xl lg:text-2xl dark:text-white">
+                CAPACÍTATE CON LOS LÍDERES DEL CONCRETO</h2>
+                <h3 className="font-montserrat pt-10 pb-2 text-white font-bold text-4xl md:text-2xl lg:text-5xl">DIPLOMADO EN TECNOLOGÍA DEL CONCRETO</h3>
+                <h4 className="font-montserrat pb-10 text-white text-2xl">INICIA EL 30 DE OCTUBRE 2025</h4>
+                <p className="font-montserrat mb-16 text-xs font-light text-white lg:text-xl sm:px-14 lg:px-24 dark:text-gray-200">Forma parte del diplomado que construye el conocimiento del mañana.<br/>10 módulos mensuales hasta octubre 2026.
+                </p>
+                <Link
+                  href="/eventos/diplomado-tecnologia-del-concreto"
+                  className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-2 text-sm text-blue-700 rounded-full bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 mr-3"
+                >
+                  <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
+                    MÁS INFORMACIÓN
+                  </span>{" "}
+                  <svg
+                    className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 9 4-4-4-4"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </SwiperSlide>
+            
           </Swiper>
         </section>
       </main>
