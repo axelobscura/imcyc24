70c144d1b5d47be25f52ec998cb5d313bbac5d07
Axel Laurent <axosar@gmail.com>
2026-01-20 17:31:30 -0600

    members

--- src/app/membresias/page.tsx
+++ src/app/membresias/page.tsx
@@ -5,7 +5,7 @@ import { FaMobile } from "react-icons/fa6";
 import { IoIosAlert } from "react-icons/io";
 import { BsCreditCard2FrontFill } from "react-icons/bs";
 import { IoShieldCheckmark } from "react-icons/io5";
-
+import { SlClose } from "react-icons/sl";
 
 export default function Membresias() {
 
@@ -108,30 +108,36 @@ export default function Membresias() {
                 <h2 className="font-montserrat text-4xl font-bold text-white pt-0 mb-0 pb-0 text-center">Métodos de Pago Seguros</h2>
                 <p className="text-white text-center">Elige la forma de pago que más te convenga. Todos nuestros pagos son 100% seguros.</p>
                 <div className="my-5 grid grid-cols-1 sm:grid-cols-[1fr_1fr_1fr_1fr] md:grid-cols-4 gap-6 text-white font-montserrat text-center mt-7">
-                    <div className="bg-slate-900 border border-slate-500 p-3 py-5 rounded-[30px] flex flex-col justify-center items-center">
+                    <div className="bg-slate-900 border border-slate-500 p-3 py-5 rounded-[30px] flex flex-col items-center">
                         <BsCreditCard2FrontFill size={30} className="mb-3" />
                         <p className="font-bold">Tarjetas de Crédito</p>
                         <p>Visa, MasterCard, American Express</p>
                     </div>
-                    <div className="bg-slate-900 border border-slate-500 p-3 rounded-[30px] flex flex-col justify-center items-center">
+                    <div className="bg-slate-900 border border-slate-500 p-3 pt-4 rounded-[30px] flex flex-col items-center">
                         <FaMobile size={30} className="mb-3" />
                         <p className="font-bold">Pagos Móviles</p>
                         <p>Apple Pay, Google Pay, PayPal</p>
                     </div>
-                    <div className="bg-slate-900 border border-slate-500 p-3 rounded-[30px] flex flex-col justify-center items-center">
+                    <div className="bg-slate-900 border border-slate-500 p-3 rounded-[30px] flex flex-col items-center">
                         <BsCreditCard2FrontFill size={30} className="mb-3" />
                         <p className="font-bold">Transferencia Bancaria</p>
                         <p>Transferencias locales e internacionales</p>
                     </div>
-                    <div className="bg-slate-900 border border-slate-500 p-3 rounded-[30px] flex flex-col justify-center items-center">
+                    <div className="bg-slate-900 border border-slate-500 p-3 rounded-[30px] flex flex-col items-center">
                         <IoShieldCheckmark size={30} className="mb-3" />
                         <p className="font-bold">Pago Seguro</p>
                         <p>Encriptación SSL de 256 bits</p>
                     </div>
                 </div>
-                <div className="bg-slate-900 p-5 rounded-[20px] mt-10">
-                    <h2 className="font-montserrat text-4xl font-bold text-white pt-0 mb-0 pb-0">Información Importante</h2>
-                    <p className="text-white">Lo que NO incluyen los planes</p>
+                <div className="bg-blue-900 p-5 rounded-[20px] mt-10">
+                    <h2 className="font-montserrat text-4xl font-bold text-white pt-0 mb-0 pb-0 flex items-center"><IoIosAlert className="mr-2" /> Información Importante</h2>
+                    <p className="text-white my-3 font-bold">Lo que NO incluyen nuestros planes:</p>
+                    <p className="text-white flex items-center"><SlClose className="mr-3" size={15} /> Constancias de aptitud profesional</p>
+                    <p className="text-white flex items-center"><SlClose className="mr-3" size={15} /> Certificaciones oficiales de competencia</p>
+                    <p className="text-white flex items-center"><SlClose className="mr-3" size={15} />Exámenes de certificación técnica</p>
+                    <div className="my-5 bg-slate-500 p-3 px-5 rounded-[20px]">
+                        <p className="text-white font-bold">Los planes se enfocan en capacitación y desarrollo profesional. Para obtener certificaciones oficiales y ensayos de aptitud, consulta nuestros servicios especializados.</p>
+                    </div>
                 </div>
             </div>
             </div>
