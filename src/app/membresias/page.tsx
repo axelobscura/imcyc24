8a8aedeac377c267f6cac41ee293b4e0900d4a6b
Axel Laurent <axosar@gmail.com>
2026-01-20 17:35:40 -0600

    members

--- src/app/membresias/page.tsx
+++ src/app/membresias/page.tsx
@@ -11,7 +11,7 @@ export default function Membresias() {
 
     return (
         <div>
-            <div className="grid grid-rows-[1fr] pb-20 gap-16 sm:p-20 bg-gray-900 bg-gradient-to-t from-gray-800 to-slate-900 font-[family-name:var(--font-geist-sans)] z-10 bg-cover bg-center bg-no-repeat py-40 sm:py-40">
+            <div className="grid grid-rows-[1fr] pb-20 gap-16 sm:p-20 bg-gray-900 bg-gradient-to-t from-gray-900 to-slate-700 font-[family-name:var(--font-geist-sans)] z-10 bg-cover bg-center bg-no-repeat py-40 sm:py-40">
                 <div className="mx-auto max-w-screen-xl px-5">
                 <main className="pt-20">
                     <h1 className="font-montserrat text-4xl font-bold text-white pt-0 mb-0 pb-0 text-center">MEMBRESÍAS IMCYC</h1>
