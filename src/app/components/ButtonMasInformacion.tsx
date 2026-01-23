db9cc2c3ff36bccb431d8ab731a1fc616a875be4
Axel Laurent <axosar@gmail.com>
2026-01-22 18:10:24 -0600

    membresias

--- src/app/components/ButtonMasInformacion.tsx
+++ src/app/components/ButtonMasInformacion.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default function ButtonMasInformacion() {
   return (
-      <Link href="https://wa.me/5636853914" target="_blank" className="w-full bg-blue-500 hover:bg-slate-600 transition-all ease-in-out text-white text-center flex justify-center items-center p-2 rounded-[10px] font-bold">
+      <Link href="https://wa.me/5529098047" target="_blank" className="w-full bg-blue-500 hover:bg-slate-600 transition-all ease-in-out text-white text-center flex justify-center items-center p-2 rounded-[10px] font-bold">
         <FaWhatsapp size={30} className="mr-2" /> MÁS INFORMACIÓN 
       </Link>
   );
