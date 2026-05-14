50dd11e69ce847a259f8194b52994331f971915d
Axel Laurent <axosar@gmail.com>
2026-05-14 16:50:12 -0600

    pena

--- src/app/prensa/[articulo]/page.tsx
+++ src/app/prensa/[articulo]/page.tsx
@@ -31,7 +31,7 @@ export default function Articulo() {
 
   return (
       <div className="flex items-center justify-items-center p-0 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)] bg-[url('/libreria.png')] bg-gray-700 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat">
-        <div className="mx-auto w-full max-w-screen-xl p-4 md:pt-40 sm:pt-0 pb-0 lg:py-40">
+        <div className="mx-auto w-full max-w-screen-xl p-4 md:pt-40 sm:pt-0 pb-0 lg:py-40 mt-20">
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start z-0 w-full">
             <div className="grid grid-cols-[1fr] sm:grid-cols-[5fr_1fr] gap-0 items-start justify-items-stretch pt-20 w-full">
               <div className="block sm:hidden">
