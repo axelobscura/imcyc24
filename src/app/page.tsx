3de65d4cdb4f0990fc814f41a71e00b4224e4a4d
Axel Laurent <axosar@gmail.com>
2026-06-26 09:26:33 -0600

    banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,48 +39,6 @@ export default function Home() {
             }}
           >
             <SwiperSlide 
-              className="bg-[url('/banner/pisos.jpg')] bg-gray-500 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat h-auto"
-              style={{ height: '100vh' }}
-            >
-                <Link
-                  href="/eventos/examen-aci-tecnico-y-acabador-de-superficies-planas-de-concreto-certifica-tu-conocimiento-tecnico-en-la-colocacion-compactado-extendido-curado-y-acabado-profesional-de-las-superficies-planas-de-concreto-bajo-estandares-internacionales"
-                >
-                <div className="py-0 px-0 mx-auto max-w-screen-xl text-center z-10 relative pb-36 pt-36">
-                  <h1 className="font-montserrat pt-20 pb-0 text-white text-2xl mb-5">INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
-                  <Image src="/banner/logo_aci.png" alt="TÉCNICO Y ACABADOR DE SUPERFICIES PLANAS DE CONCRETO" width={200} height={100} className="my-5" />
-                  <h3 className="font-montserrat pt-5 pb-0 text-5xl mb-3 text-purple-400 font-bold">EXAMEN ACI</h3>
-                  <h2 className="font-montserrat mb-0 text-3xl font-light tracking-tight leading-none text-white md:text-4xl lg:text-6xl dark:text-white">
-                  TÉCNICO Y ACABADOR DE SUPERFICIES PLANAS DE CONCRETO</h2>
-                  <p className="text-2xl font-montserrat py-5 text-purple-300">Certifica tu conocimiento técnico en la colocación, compactado, extendido, curado y acabado profesional de las superficies planas de concreto bajo estándares internacionales</p>
-                  <p className="text-2xl font-montserrat py-1 text-purple-200 font-bold">Al aprobar obtienes Certificado ACI</p>
-                  <p className="text-white text-2xl font-montserrat py-3 pb-1">DURACIÓN: 5h de 9h a 14h</p>
-                  <p className="text-white text-2xl font-montserrat py-1 pb-3 font-bold">COSTO: $8,000.00 MXN + IVA</p>
-                  <p className="bg-yellow-300 bg-opacity-80 b-3 text-2xl p-2 rounded-full text-white font-bold">INCLUYE SESIÓN TÉCNICA PREVIA, DE 9 A 12 H</p>
-                  <h3 className="font-montserrat text-4xl font-bold text-purple-400 mb-5 mt-5">25 DE JUNIO 2026</h3>
-                
-                    <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
-                      Más información
-                    </span>{" "}
-                    <svg
-                      className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 6 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="m1 9 4-4-4-4"
-                      />
-                    </svg>
-                  
-                </div>
-              </Link>
-            </SwiperSlide>
-            <SwiperSlide 
               className="bg-[url('/banner/pisos2.jpg')] bg-gray-500 bg-blend-multiply z-10 bg-cover bg-center bg-no-repeat h-auto"
               style={{ height: '100vh' }}
             >
@@ -95,7 +53,7 @@ export default function Home() {
                   <h2 className="font-montserrat mb-0 text-3xl font-light tracking-tight leading-none text-white md:text-4xl lg:text-6xl dark:text-white uppercase">
                   Pisos Industriales de Concreto<br/>Innovación que sostiene a la industria.</h2>
                   <p className="text-white text-sm pt-3">El Instituto Mexicano del Cemento y del Concreto (IMCYC), en colaboración con la Cámara Mexicana de la Industria de la Construcción, Yucatán (CMIC), presenta el Seminario: Pisos Industriales de Concreto, un espacio técnico de alto nivel diseñado para analizar los componentes críticos que garantizan la durabilidad y operatividad de las superficies de alto desempeño.</p>
-                  <h3 className="font-montserrat text-4xl font-bold text-red-700 mb-5 mt-5">29 y 30 de Julio 2026</h3>
+                  <h3 className="font-montserrat text-4xl font-bold text-gray-300 mb-3 mt-5">29 y 30 de Julio 2026</h3>
                   <p className="text-2xl font-montserrat py-5 text-gray-200 font-bold">CMIC Yucatán, C. 50 310-por 21, Roma, 97218 Mérida, Yuc.</p>
                   <p className="text-white text-2xl font-montserrat py-1 pb-3 font-bold">COSTO: $1,500.00 MXN + IVA</p>
                     <span className="text-xs bg-red-900 rounded-full text-white px-4 py-1.5 mt-3">
